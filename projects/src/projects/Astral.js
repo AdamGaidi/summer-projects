f5647f19347016510c8c0d364b8c4c46d5002757
Adam Gaidi <adamgaidi@outlook.com>
2020-08-23 19:29:33 +0200

    commiting all changes for computer change

--- projects/src/projects/Astral.js
+++ projects/src/projects/Astral.js
@@ -44,16 +44,7 @@ const Astral = forwardRef((props, ref) => {
             text="Explore the unknown"
             styling={"takeoff-header"}
           />
-          <AstralFadeText
-            styling={"takeoff-text"}
-            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum."
-          />
+          <AstralFadeText styling={"takeoff-text"} text="" />
         </div>
         <img src={takeoff} id="astral-image-left" alt="rocket launch" />
       </div>
