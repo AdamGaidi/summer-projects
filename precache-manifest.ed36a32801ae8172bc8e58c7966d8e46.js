a42d6f0a2ad2a3d8fb39efe371237423985f82b4
Adam Gaidi <adamgaidi@outlook.com>
2020-09-03 20:17:32 +0200

    Updates

---
+++ precache-manifest.ed36a32801ae8172bc8e58c7966d8e46.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf1b51c17b59bdd74af576c1cd2eda5",
+    "url": "/summer-projects/index.html"
+  },
+  {
+    "revision": "cc7a3ccd5fca80aa8cd3",
+    "url": "/summer-projects/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "fc0b94e6288e4f6018f5",
+    "url": "/summer-projects/static/css/main.84265a5f.chunk.css"
+  },
+  {
+    "revision": "cc7a3ccd5fca80aa8cd3",
+    "url": "/summer-projects/static/js/2.930f2387.chunk.js"
+  },
+  {
+    "revision": "a73511fdd0e79fea52c5f42ad1574633",
+    "url": "/summer-projects/static/js/2.930f2387.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc0b94e6288e4f6018f5",
+    "url": "/summer-projects/static/js/main.ecf4804a.chunk.js"
+  },
+  {
+    "revision": "0372a30cbd30ac776863",
+    "url": "/summer-projects/static/js/runtime-main.e20273b2.js"
+  },
+  {
+    "revision": "a19acb3ee32051e52e59c3214675f0f9",
+    "url": "/summer-projects/static/media/artificial-intelligence.a19acb3e.svg"
+  },
+  {
+    "revision": "ddce126599512a5c7c49a31492b8274e",
+    "url": "/summer-projects/static/media/coding.ddce1265.svg"
+  },
+  {
+    "revision": "ea7182310b65eee4d188e9e03bcd9228",
+    "url": "/summer-projects/static/media/igor-karimov-9AmKnNZw3GA-unsplash.ea718231.jpg"
+  },
+  {
+    "revision": "8d0bd35c98eb648b612e2897f41efa50",
+    "url": "/summer-projects/static/media/johny-goerend-Oz2ZQ2j8We8-unsplash.8d0bd35c.jpg"
+  },
+  {
+    "revision": "9e500ee882090fcb480d983ede381034",
+    "url": "/summer-projects/static/media/nasa-OLlj17tUZnU-unsplash.9e500ee8.jpg"
+  },
+  {
+    "revision": "27a57425dc8f553108fef7dd5101c3c3",
+    "url": "/summer-projects/static/media/nasa-n463SoeSiVY-unsplash.27a57425.jpg"
+  },
+  {
+    "revision": "1b66fae646e6c2806bc707be1c2ef9ae",
+    "url": "/summer-projects/static/media/nasa-yZygONrUBe8-unsplash.1b66fae6.jpg"
+  },
+  {
+    "revision": "fc6225f160a65b7910430150f864dfd7",
+    "url": "/summer-projects/static/media/paper.fc6225f1.png"
+  }
+]);
